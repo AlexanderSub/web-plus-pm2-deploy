82c44738b7cad88cb98961033d12f82342176d83
Alexander <subbsan@gmail.com>
2023-03-18 23:14:46 +0300

    deploy

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -22,8 +22,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
-      'post-deploy': 'npm i && npm run build',
+      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
+      'post-deploy': `cd ${DEPLOY_PATH}/source/backend && npm i && npm run build && pm2 restart all`,
     },
   },
 };
