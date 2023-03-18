abb24848b4328dbd84b55dd248420d7eebb352d0
Alexander <subbsan@gmail.com>
2023-03-18 23:12:02 +0300

    deploy

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -12,7 +12,7 @@ const {
 module.exports = {
   apps: [{
     name: 'mesto-project',
-    script: './app.js',
+    script: './dist/app.js',
   }],
 
   deploy: {
@@ -22,8 +22,8 @@ module.exports = {
       ref: DEPLOY_REF,
       repo: DEPLOY_REPO,
       path: DEPLOY_PATH,
-      'pre-deploy-local': `scp ./.env* ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/backend`,
-      'post-deploy': 'cd ~/web-plus-pm2-deploy/backend/ && npm i && npm run build && pm2 restart ecosystem.config.js',
+      'pre-deploy': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
     },
   },
 };
